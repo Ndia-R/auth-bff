874fff559b0f21a6f6bd397b5c69a45d489127c5
ubuntu <ubuntu@gmail.com>
2025-09-21 08:31:31 +0900

    first commit

---
+++ src/main/java/com/example/auth_bff/AuthBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthBffApplication.class, args);
+	}
+
+}
